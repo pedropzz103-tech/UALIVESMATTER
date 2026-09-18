e22fb3a7fe0aa081926809d3550f6adeb244e527
pedropzz103-tech <pedropzz103@gmail.com>
2026-09-18 19:25:57 +0200

    Bump redesigned app to v0.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.ualivesmatter.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.4.0"
+        versionCode = 5
+        versionName = "0.5.0"
     }
 
     buildTypes {
