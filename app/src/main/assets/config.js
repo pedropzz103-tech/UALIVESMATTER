afecb4674306aee22be0055d2af85e5b88bbbe62
pedropzz103-tech <pedropzz103@gmail.com>
2026-09-18 18:50:49 +0200

    Add v0.2 app/src/main/assets/config.js

---
+++ app/src/main/assets/config.js
@@ -0,0 +1,7 @@
+window.UA_CONFIG = {
+  supabaseUrl: "",
+  supabaseAnonKey: "",
+  airAlertsProxy: "",
+  kyivSheltersUrl: "https://gisserver.kyivcity.gov.ua/mayno/rest/services/KYIV_API/Public_protection/MapServer/0/query?where=1%3D1&outFields=*&returnGeometry=true&f=geojson",
+  overpassUrl: "https://overpass-api.de/api/interpreter"
+};
