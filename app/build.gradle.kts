d9ccdaa17438c97ae54b3a755ec7ab96ab573aa7
pedropzz103-tech <pedropzz103@gmail.com>
2026-09-18 21:37:03 +0200

    Bump startup hotfix to v0.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.ualivesmatter.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.7.0"
+        versionCode = 9
+        versionName = "0.7.1"
     }
 
     buildTypes {
