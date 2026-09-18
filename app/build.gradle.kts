23ff3d15bcab1174d437a0d633ac592149f1f53b
pedropzz103-tech <pedropzz103@gmail.com>
2026-09-18 21:19:57 +0200

    Bump polished app to v0.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.ualivesmatter.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.6.1"
+        versionCode = 8
+        versionName = "0.7.0"
     }
 
     buildTypes {
