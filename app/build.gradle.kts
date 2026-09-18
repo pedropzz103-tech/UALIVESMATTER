2bc56664c7eac0d1d0e025a355210db345156710
pedropzz103-tech <pedropzz103@gmail.com>
2026-09-18 20:44:37 +0200

    Bump app to v0.6.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.ualivesmatter.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.6.0"
+        versionCode = 7
+        versionName = "0.6.1"
     }
 
     buildTypes {
