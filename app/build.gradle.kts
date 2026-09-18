a70dd13c465ff55756543d881e74eaf58bfbd6ad
pedropzz103-tech <pedropzz103@gmail.com>
2026-09-18 18:54:40 +0200

    Bump app version to 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.ualivesmatter.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
     }
 
     buildTypes {
